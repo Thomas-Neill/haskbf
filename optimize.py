fbb13b6f936357901ccdd190eb7e85efd6f0f26a
Tommy <tommydneill@gmail.com>
2017-07-06 05:27:19 -0700

    Started over again from scratch; and am making this a Haskell DSL instead.

--- optimize.py
+++
@@ -1,8 +0,0 @@
-import re
-def optimize(brainfuck):
-    stupidPatterns = ["<>","><","\+-","-\+"]
-    for bad in stupidPatterns:
-        while(re.search(bad,brainfuck)):
-            brainfuck = re.sub(bad,'',brainfuck)
-    return brainfuck
-
